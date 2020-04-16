bf322a318f615146429b382088d43c2a5711da6c
Naga <nsudheer.droiddev@gmail.com>
2020-04-16 10:56:14 +0530

    init commit for all

---
+++ app/src/main/java/com/sudheer/dailynewsforu/acitvities/ui/Headlines/HeadLinesFragment.java
@@ -0,0 +1,55 @@
+package com.sudheer.dailynewsforu.acitvities.ui.Headlines;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sudheer.dailynewsforu.R;
+import com.sudheer.dailynewsforu.adapters.HeadlinesAdapter;
+import com.sudheer.dailynewsforu.models.HeadlineModel;
+
+import java.util.List;
+
+public class HeadLinesFragment extends Fragment {
+
+    private RecyclerView recyclerViewHeadline;
+    private HeadlinesAdapter headlinesAdapter;
+    private HeadLinesViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HeadLinesViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_headlines, container, false);
+        //   final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.init();
+      /*  homeViewModel.getHeadlinesList().observe(getViewLifecycleOwner(), new Observer<Object>() {
+            @Override
+            public void onChanged(List<HeadlineModel> headLinesViewModels) {
+
+            }
+        });*/
+      homeViewModel.getHeadlinesList().observe(getViewLifecycleOwner(), new Observer<List<HeadlineModel>>() {
+          @Override
+          public void onChanged(List<HeadlineModel> headlineModels) {
+              headlinesAdapter = new HeadlinesAdapter(getContext(), homeViewModel.getHeadlinesList().getValue());
+              recyclerViewHeadline = root.findViewById(R.id.rec_headline_);
+              RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+              recyclerViewHeadline.setLayoutManager(layoutManager);
+              recyclerViewHeadline.setAdapter(headlinesAdapter);
+              headlinesAdapter.notifyDataSetChanged();
+          }
+      });
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/sudheer/dailynewsforu/acitvities/ui/Headlines/HeadLinesViewModel.java
@@ -0,0 +1,101 @@
+package com.sudheer.dailynewsforu.acitvities.ui.Headlines;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.sudheer.dailynewsforu.interfaces.UserConsentInterface;
+import com.sudheer.dailynewsforu.models.HeadlineModel;
+import com.sudheer.dailynewsforu.repository.HeadLinesRepo;
+import com.sudheer.dailynewsforu.utils.RetrofitSSLCertificate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class HeadLinesViewModel extends ViewModel {
+
+    HeadLinesRepo headLinesRepo;
+    private MutableLiveData<List<HeadlineModel>> listHeadlineMutableLiveData;
+
+    public HeadLinesViewModel() {
+        listHeadlineMutableLiveData = new MutableLiveData<List<HeadlineModel>>();
+        //listMutableLiveData.setValue("This is Headlines fragment");
+
+    }
+
+    public void init() {
+
+      /*  if (listHeadlineMutableLiveData != null) {
+            return;
+        }
+*/
+        getlistData();
+        // headLinesRepo = HeadLinesRepo.getInstance();
+        // listHeadlineMutableLiveData = headLinesRepo.getHeadlineList();
+
+
+    }
+
+    /*Retrofit Service Calling for Login*/
+    private UserConsentInterface getInterfaceCheckService() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://newsapi.org/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                //this is for ssl certificate
+                .client(RetrofitSSLCertificate.getUnsafeOkHttpClient())
+                .build();
+        final UserConsentInterface mInterfaceService = retrofit.create(UserConsentInterface.class);
+        return mInterfaceService;
+    }
+
+    public LiveData<List<HeadlineModel>> getHeadlinesList() {
+        return listHeadlineMutableLiveData;
+    }
+
+    public void getlistData() {
+        Log.d("service ", "doinf service call");
+        UserConsentInterface mApiService = this.getInterfaceCheckService();
+        try {
+
+
+            Call<HeadlineModel> mService = mApiService.getUserConsents("in", "e23f1c9b8dcc447ab0c227c737e0a13c");
+            try {
+                mService.enqueue(new Callback<HeadlineModel>() {
+
+                    @Override
+                    public void onResponse(Call<HeadlineModel> call, Response<HeadlineModel> response) {
+                        List<HeadlineModel> headlineModels = new ArrayList<>();
+                        headlineModels.add(response.body());
+                       /* for(int i=0;i<response.body().getTotalResults();i++){
+
+                        }*/
+
+
+
+                        listHeadlineMutableLiveData.setValue(headlineModels);
+                        Log.d("service ", "datacame" + response.code());
+                        //  listHeadlineMutableLiveData.setValue(response);
+                    }
+
+                    @Override
+                    public void onFailure(Call<HeadlineModel> call, Throwable t) {
+                        Log.d("service ", "faild");
+                    }
+                });
+            } catch (Exception e) {
+                e.getMessage();
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+}
